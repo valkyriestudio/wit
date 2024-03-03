4ec4f9be81fa54f544ad41475cd1b1a80e0f3aa7
vac (Brendan) <dot.fun@protonmail.com>
2024-03-03 21:39:47 +0800

    chore: make project less opinionated (#4)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use axum::{http::StatusCode, routing::get, Router};
-use mimalloc::MiMalloc;
 use std::net::SocketAddr;
+
+use axum::{http::StatusCode, routing::get, Router};
+use env_logger::Env;
+use mimalloc::MiMalloc;
 use tower_http::{
-  trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer},
-  LatencyUnit,
+    trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer},
+    LatencyUnit,
 };
 use tracing::Level;
 
@@ -11,48 +13,48 @@ use tracing::Level;
 static GLOBAL: MiMalloc = MiMalloc;
 
 fn create_app() -> Router {
-  let app = Router::new()
-    .route("/", get(health))
-    .layer(
-      TraceLayer::new_for_http()
-        .make_span_with(DefaultMakeSpan::new().include_headers(true))
-        .on_request(DefaultOnRequest::new().level(Level::INFO))
-        .on_response(
-          DefaultOnResponse::new()
-            .level(Level::INFO)
-            .latency_unit(LatencyUnit::Micros),
-        ),
-    )
-    .route("/healthz", get(health));
-
-  app
+    Router::new()
+        .route("/", get(health))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::new().include_headers(true))
+                .on_request(DefaultOnRequest::new().level(Level::INFO))
+                .on_response(
+                    DefaultOnResponse::new()
+                        .level(Level::INFO)
+                        .latency_unit(LatencyUnit::Micros),
+                ),
+        )
+        .route("/healthz", get(health))
 }
 
 #[tokio::main]
 async fn main() {
-  env_logger::init();
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
-  let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
-  let app = create_app();
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    let app = create_app();
 
-  tracing::info!("listening on {}", addr);
+    tracing::info!("listening on {}", addr);
 
-  let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-  axum::serve(listener, app).await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
 
-pub async fn health() -> StatusCode {
-  StatusCode::OK
+async fn health() -> StatusCode {
+    StatusCode::OK
 }
 
 #[cfg(test)]
 mod tests {
-  use axum::{http::StatusCode, response::IntoResponse};
+    use super::*;
 
-  #[tokio::test]
-  async fn test_health() {
-    let response = crate::health().await.into_response();
+    use axum::response::IntoResponse;
 
-    assert!(response.status() == StatusCode::OK);
-  }
+    #[tokio::test]
+    async fn test_health() {
+        let response = health().await.into_response();
+
+        assert!(response.status() == StatusCode::OK);
+    }
 }
