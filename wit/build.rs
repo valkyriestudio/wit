f044c36e94cdb80ebea20d74958a853477d480f0
fallenwood <fallenwood.y@outlook.com>
2024-03-17 11:50:54 +0800

    run install before build

--- wit/build.rs
+++ wit/build.rs
@@ -2,8 +2,18 @@ use std::io;
 use std::process::{ExitStatus, Command};
 
 fn main() {
-    build_tailwind()
+    install_node_packages()
+        .and_then(|_| {
+            build_tailwind()
+        })
         .expect("Failed to build tailwindcss...");
+}
+
+#[cfg(target_os = "windows")]
+fn install_node_packages() -> io::Result<ExitStatus> {
+    Command::new("yarn.cmd")
+        .arg("install")
+        .status()
 }
 
 #[cfg(target_os = "windows")]
@@ -11,6 +21,13 @@ fn build_tailwind() -> io::Result<ExitStatus> {
     Command::new("yarn.cmd")
         .arg("run")
         .arg("build")
+        .status()
+}
+
+#[cfg(target_os = "linux")]
+fn install_node_packages() -> io::Result<ExitStatus> {
+    Command::new("yarn")
+        .arg("install")
         .status()
 }
 
