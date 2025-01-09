1136709e15e45e6cb9f05cdb7dcf2d0730a9ea73
vac <dot.fun@protonmail.com>
2025-01-09 22:06:19 +0800

    [skip ci] adopt biome for frontend formatting (#18)

--- wit/tailwind.config.js
+++ wit/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.html"],
+  content: ['./templates/**/*.html'],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
-}
+  plugins: [require('daisyui')],
+};
